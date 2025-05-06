c0081c577142797aceef777c0d414253d318e935
Mathos Hallis <mathosha@pr-i3.kzn.21-school.ru>
2025-05-06 14:46:55 +0300

    added handlers

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,6 @@ import (
 func main() {
 	handlers := new(handler.Handler)
 
-
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRouts()); err != nil {
 		log.Fatalf("error start server: %s", err.Error())
